47664d639f8177c0496eba10358c4764ee1ff92a
Qiu Kangbo <812751114@qq.com>
2013-09-12 14:08:36 +0800

    add source map,we can check the original js code in browser

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,228 +1,224 @@
-/*
- * grunt
- * http://gruntjs.com/
- *
- * KB
- * 
- * https://github.com/kbisnotzombie/grunt-fedeploy
- */
-
-'use strict';
-
+/*global module:false*/
 module.exports = function(grunt) {
-
-  // Project configuration.
+  var project = grunt.file.readJSON('duobei-portal-fe.json');
+  var jspSrc = project.feProjectDir + "/" +project.prod.jspDir,
+  	  jspDest = project.beProjectDir + "/" + project.prod.jspDir,	
+  	  publicResourceSrc = project.feProjectDir + "/" + project.prod.publicResource,
+  	  publicResourceDest = project.beProjectDir + "/" + project.prod.publicResource,
+  	  tomcatDir = project.prod.tomcatDir,
+  	  deployStaticDir = project.prod.webStaticDir,
+  	  deployStaticImgDir = project.prod.webImgDir,
+  	  optimizationResourceSrc = project.feProjectDir + "/static", 
+  	  lessSrc = project.feProjectDir + "/" + project.prod.lessDir,
+  	  localLessDir = project.lessDir,
+  	  lessBuildCssDest = project.feProjectDir + "/" + project.prod.lessBuildCssDir,
+  	  imgDomain = project.prod.imgDomain,
+  	  imgDir = project.imgDir,
+  	  staticDomain = project.prod.staticDomain;
+  	  
   grunt.initConfig({
-    nodeunit: {//useles
-      all: ['test/{grunt,tasks,util}/**/*.js']
+    pkg: '<duobei-portal-fe.json>',
+    
+    copy: {
+    	  jsp: {
+			  files: [
+			        {expand: true, cwd: jspSrc, src: ['**'], dest: jspDest} // makes all src relative to cwd
+//					{expand: true, cwd: 'WebContent/WEB-INF/jsp/', src: ['**'], dest: 'WebContent/WEB-INF/test/'}
+			  ]
+    	  },
+    	  copyPublic: {
+    	  	files: [
+		            {expand: true, cwd: publicResourceSrc, src: ['**'], dest: publicResourceDest} // makes all src relative to cwd
+		    ]
+      	  },
+      
+    	  copyStatic:{
+      	  		files:[
+				  {expand: true, cwd: optimizationResourceSrc+'/styles', src: ['**'], dest: deployStaticDir+'/styles/'}, // makes all css src relative to cwd
+				  {expand: true, cwd: publicResourceSrc+'/flash', src: ['**'], dest: deployStaticDir+'/flash/'}, // makes all flash src relative to cwd
+				  {expand: true, cwd: publicResourceSrc+'/javascript', src: ['**'], dest: deployStaticDir+'/javascript/'}, // makes all js src relative to cwd
+				  {expand: true, cwd: optimizationResourceSrc+'/images', src: ['**'], dest: deployStaticImgDir} // makes all img src relative to cwd
+				]
+    	  },
+    	  depoyFront:{
+    	  	files: [
+  		        {expand: true, cwd: jspDest, src: ['**'], dest: tomcatDir+'/WEB-INF/jsp/'} // makes all img src relative to cwd
+  		    ]
+    	  }
     },
-    qunit: {
-        all: ['node_modules/grunt-contrib-qunit/test/**/*.html']
-    },
-    jshint: {
-		files: ['testSrc/javascript/anonymous_dashboard_old.js'],
-	    options: { 
-	    	curly: true,
-	        eqeqeq: true,
-//	        immed: true,
-//	        latedef: true, 
-//	        newcap: true,
-	        noarg: true,
-	        sub: true,
-	        undef: true,
-	        boss: true,
-	        eqnull: true,
-	        browser: true,
-	        globals: {
-	    		jQuery: true
-	    	}
-//	        ignores: ['js/jquery.js']
-	    }
-    	
-//      gruntfile: ['Gruntfile.js'],
-//      libs_n_tests: ['lib/**/*.js', '<%= nodeunit.all %>'],
-//      subgrunt: ['<%= subgrunt.all %>'],
-//      options: {
-//        curly: true,
-//        eqeqeq: true,
-//        immed: true,
-//        latedef: true,
-//        newcap: true,
-//        noarg: true,
-//        sub: true,
-//        undef: true,
-//        unused: true,
-//        boss: true,
-//        eqnull: true,
-//        node: true,
-//        es5: true
-//      }
-    },
-    watch: {
-      gruntfile: {
-        files: ['<%= jshint.gruntfile %>'],
-        tasks: ['jshint:gruntfile']
-      },
-      libs_n_tests: {
-        files: ['<%= jshint.libs_n_tests %>'],
-        tasks: ['jshint:libs_n_tests', 'nodeunit']
-      },
-      subgrunt: {
-        files: ['<%= subgrunt.all %>'],
-        tasks: ['jshint:subgrunt', 'subgrunt']
-      }
-    },
-    subgrunt: {
-      all: ['test/gruntfile/*.js']
-    },
-    less: {//ok
+    
+    less: {
     	options: {
-    		paths: ["testSrc/less"],
     		yuicompress: true
 	    },
 	    dynamic_mappings: {
 	        files: [
 	          {
 	            expand: true,     // Enable dynamic expansion.
-	            cwd: 'testSrc/less',      // Src matches are relative to this path.
+//	            cwd: 'WebContent/resources/public/styles/less',      // Src matches are relative to this path.
+	            cwd:lessSrc,
 	            src: ['*.less'], // Actual pattern(s) to match.
-	            dest: 'testDest/css',   // Destination path prefix.
+//	            dest: 'WebContent/resources/public/tempCss',   // Destination path prefix.
+	            dest:lessBuildCssDest,
 	            ext: '.css'   // Dest filepaths will have this extension.
 	          },
 	        ]
 	    }
 	},
-	cssmin: {//compress css,ok
-		add_banner: {
-	    options: {
-	    	banner: '/* My minified css file */'
-	    },
-	    files: {
-	    	'testDest/test/output.css': ['testSrc/test/*.css']
+	
+	replace:{
+		dist:[
+			{
+				  src: optimizationResourceSrc+'/styles/',
+				  ext: '.css',
+				  replacetoken: '../../../images',
+				  replacevalue: imgDomain
+			},
+    	    {
+    	    	  src: optimizationResourceSrc+'/styles/',
+    	    	  ext: '.css',
+    	    	  replacetoken: '../../images',
+    	    	  replacevalue: imgDomain
+    	     }
+    	]
+	},
+	
+	smushit:{
+		destination:{
+//	        src: publicResourceSrc +'/images',
+//	        dest: optimizationResourceSrc+'/images'
+//			src: 'WebContent/resources/public/images',
+//	        dest: 'static/images'
+	        src: 'WebContent/resources/public/temp1',
+	        dest:'WebContent/resources/public/temp2'
 	    }
-	  }
 	},
-	copy: {//copy files and folders,ok
-		main: {
-			files: [
-//		      {expand: true, src: ['testSrc/test/*'], dest: 'testDest/', filter: 'isFile'}, // includes files in path
-//		      {expand: true, src: ['testSrc/jsp/**'], dest: 'testDest/'} // includes files in path and its subdirs
-		      {expand: true, cwd: 'testSrc/jsp/', src: ['**'], dest: 'testDest/jsp'} // makes all src relative to cwd
-//		      {expand: true, flatten: true, src: ['testSrc/**'], dest: 'testDest/', filter: 'isFile'} // flattens results to a single level
-		    ]
-		},
-		copygif :{
-			files: [
-				{expand: true, cwd: 'testSrc/images', src: ['*.gif'], dest: 'testDest/images'}, // makes all src relative to cwd
-			]
-		}
-	},
-	compress: {//compress files and folders,ok
-		  main: {
-			  options: {
-				  archive: 'archive.zip'
-			  },
-			  files: [
-//		          {src: ['testSrc/javascript/*'], dest: 'compress/', filter: 'isFile'}, // includes files in path
-//		          {src: ['path/**'], dest: 'internal_folder2/'}, // includes files in path and its subdirs
-//		          {expand: true, cwd: 'path/', src: ['**'], dest: 'internal_folder3/'}, // makes all src relative to cwd
-//		          {flatten: true, src: ['path/**'], dest: 'internal_folder4/', filter: 'isFile'} // flattens results to a single level
-			  ]
-		  }
+	
+	replaceJspStatic: {
+		dist:[
+		      {
+		    	  src: jspDest+ '/',
+    	    	  ext: '.jsp'
+		      }
+		],
+		lessDist: optimizationResourceSrc + "/styles",
+		jsDist: publicResourceSrc +"/javascript/lib",
+		staticDomain: staticDomain
 	},
 	concat: {//concatenate files,ok
 	    options: {
 	      separator: ';'
-	    },
-	    dist: {
-	      src: ['testSrc/javascript/dashboard.js', 'testSrc/javascript/course_area.js', 'testSrc/javascript/createPm.js'],
-	      dest: 'testDest/test/built.js'
 	    }
+//	    dist: {
+//	      src: ['testSrc/javascript/dashboard.js', 'testSrc/javascript/course_area.js', 'testSrc/javascript/createPm.js'],
+//	      dest: 'testDest/test/built.js'
+//	    }
 	},
 	uglify: {//minify files with uglify,ok
-	    my_target: {
-	      files: {
-	        'testDest/test/output.min.js': ['testSrc/javascript/dashboard.js', 'testSrc/javascript/course_area.js']
-	      }
-	    }
+//	    my_target: {
+//	      files: {
+//	        'testDest/test/output.min.js': ['testSrc/javascript/dashboard.js', 'testSrc/javascript/course_area.js']
+//	      }
+//	    }
 	},
-	imagemin: {// Task,minify the images,ok
-	    dist: {                            // Target
-	      options: {                       // Target options
-	        optimizationLevel: 3
-	      },
-	      files: [
-              {
-                expand: true,     // Enable dynamic expansion.
-                cwd: 'testSrc/images',      // Src matches are relative to this path.
-                src: ['*.png'], // Actual pattern(s) to match.
-                dest: 'testDest/images',   // Destination path prefix.
-                ext: '.png',   // Dest filepaths will have this extension.
-              },
-              {
-                  expand: true,     // Enable dynamic expansion.
-                  cwd: 'testSrc/images',      // Src matches are relative to this path.
-                  src: ['*.jpg'], // Actual pattern(s) to match.
-                  dest: 'testDest/images',   // Destination path prefix.
-                  ext: '.jpg',   // Dest filepaths will have this extension.
-              }
-	      ],
-	    }
-	},
-	findUselessImg:{
-	    	dist:{
-				src:imgDir, 
-				lessDir:localLessDir,
-				localJspDir:'WebContent/WEB-INF/jsp/portal'
-			}
-	    },
 	concatMin:{
 		jspSrc:{
-			 src: 'testDest/jsp',
-	    	 ext: '.jsp'
+//			 src: 'testDest/jsp',
+			 src: jspDest+ '/',
+	    	 ext: '.jsp', 
+	    	 jsLocation: publicResourceSrc+'/javascript/lib',
+	    	 concatJsDest:publicResourceSrc + '/javascript/concatJs',
+	    	 uglifiedJsDest:publicResourceSrc+'/javascript/lib',
+	    	 staticDomain:staticDomain
 		}
-	}
+	},
+	changeSourceMapPath:{
+		targetSrc:{
+			staticSrc:deployStaticDir+'/javascript/lib',
+			staticDomain:staticDomain
+		}
+	},
+	findUselessImg:{
+    	dist:{
+			src:imgDir, 
+			lessDir:localLessDir,
+			localJspDir:'WebContent/WEB-INF/jsp/portal'
+		}
+	},
+	cssmin: {
+	  minify: {
+	    expand: true,
+	    cwd: lessBuildCssDest + '/',
+	    src: ['*.css'],
+	    dest: lessBuildCssDest + '/',
+	    ext: '.min.css'
+	  }
+	},
+	
+	min: {
+		files: {
+	      src: [
+	      '<%= project.prod.publicResource %>/javascript/lib/*.js'
+	      ],
+	      dest: 'test/'
+	    }
+	},
+	    
+    concat: {
+      dist: {
+        src: ['<banner:meta.banner>', '<file_strip_banner:src/<%= pkg.name %>.js>'],
+        dest: 'dist/<%= pkg.name %>.js'
+      }
+    },
+    
+	mincss:{
+		dist:{
+			src: '<%= project.cssDir %>/*.css',
+			dest: '<%= project.mincssDir %>/all.min.css'
+		}
+	},
+	
+    qunit: {
+      files: ['test/**/*.html']
+    },
+    lint: {
+    	 files: ['grunt.js', '<%= project.jsDir %>/**/*.js']
+    },
+    watch: {
+      files: '<config:lint.files>',
+      tasks: 'lint qunit'
+    },
+    
+    jshint: {
+      options: {
+        curly: true,
+        eqeqeq: true,
+        immed: true,
+        latedef: true,
+        newcap: true,
+        noarg: true,
+        sub: true,
+        undef: true,
+        boss: true,
+        eqnull: true,
+        browser: true
+      },
+      globals: {
+        jQuery: true
+      }
+    },
+    uglify: {}
+    
   });
 
-  // These plugins provide necessary tasks.
-  grunt.loadNpmTasks('grunt-contrib-jshint');
-  grunt.loadNpmTasks('grunt-contrib-nodeunit');
-  grunt.loadNpmTasks('grunt-contrib-watch');
-  grunt.loadNpmTasks('grunt-contrib-qunit');
+  // Default task.
+  grunt.registerTask('prod', ['copy:jsp', 'less', 'cssmin', 'replace', 'concatMin', 'replaceJspStatic', 'copy:copyStatic', 'changeSourceMapPath']);
+  grunt.registerTask('deploy', ['copy:jsp', 'less', 'cssmin', 'replace', 'concatMin', 'replaceJspStatic', 'copy:copyStatic', 'changeSourceMapPath','copy:depoyFront']);
   
   grunt.loadNpmTasks('grunt-contrib'); 
   grunt.loadNpmTasks('grunt-hashres');
   grunt.loadNpmTasks('grunt-smushit');
   
-  grunt.loadTasks('tasks');
-  
-  // "npm test" runs these tasks
-  grunt.registerTask('test', ['jshint', 'nodeunit', 'subgrunt']);
-  
-  //压缩png、jpg，并把gif复制到目标目录，因为gif没有压缩
-  grunt.registerTask('minandcopy', ['imagemin', 'copy:copygif']);
-
-  // Default task.
-  grunt.registerTask('default', ['test']);
-  grunt.registerTask('lesstest', ['less:production']);
-
-  // Run sub-grunt files, because right now, testing tasks is a pain.
-  grunt.registerMultiTask('subgrunt', 'Run a sub-gruntfile.', function() {
-    var path = require('path');
-    grunt.util.async.forEachSeries(this.filesSrc, function(gruntfile, next) {
-      grunt.util.spawn({
-        grunt: true,
-        args: ['--gruntfile', path.resolve(gruntfile)],
-      }, function(error, result) {
-        if (error) {
-          grunt.log.error(result.stdout).writeln();
-          next(new Error('Error running sub-gruntfile "' + gruntfile + '".'));
-        } else {
-          grunt.verbose.ok(result.stdout);
-          next();
-        }
-      });
-    }, this.async());
-  });
-
+  grunt.loadTasks( 'tasks');
 };
+
--- tasks/grunt-concat-min.js
+++ tasks/grunt-concat-min.js
@@ -21,40 +21,53 @@ module.exports = function(grunt){
 			var jsLocation = jspSrc.jsLocation;
 			var concatJsDest = jspSrc.concatJsDest;
 			var uglifiedJsDest = jspSrc.uglifiedJsDest;
+			var staticDomain = jspSrc.staticDomain;
 			var i = 1;
 			
 			grunt.file.recurse(jspSrc.src,function(abspath, rootdir, subdir, filename){
 				 var extname = path.extname(filename);
 				 var filenameWithoutExt = filename;
 				 filenameWithoutExt = filenameWithoutExt.replace(extname,'');
-				 if(extname == ext){
+				 if(extname === ext && filenameWithoutExt !== 'footer'){
 					 try{
 						 var destContents = fs.readFileSync(abspath, "UTF-8");
 						 var jsContent = getMatches(destContents,/lib\/(.+).js.*?"/g, 1);
 						 var concatedJs = [];
+//						 grunt.log.ok(filename+':'+jsContent);
 						 
 						 if(jsContent.length > 0){
-							//not set js src and uglified path
 							 jsContent.forEach(function(t){
-								 concatedJs.push('testSrc/javascript/' + t + '.js'); 
+								 concatedJs.push(jsLocation + '/' + t + '.js'); 
 							 });
-							 grunt.log.ok(concatedJs); 
+//							 grunt.log.ok(concatedJs); 
 							 grunt.config.set('concat.dist' + i + '.src',concatedJs);
-							 grunt.config.set('concat.dist' + i + '.dest','testDest/javascript/' + filenameWithoutExt + '.js');
-							 grunt.config.set('uglify.my_target' + i + '.src','testDest/javascript/' + filenameWithoutExt + '.js');
-							 grunt.config.set('uglify.my_target' + i + '.dest','testDest/javascript.min/' + filenameWithoutExt + 'MIN.js');
+							 grunt.config.set('concat.dist' + i + '.dest',concatJsDest+ '/' + filenameWithoutExt + '.js');
 							 
+//							 sourceMap set start.  create new map use function(path){return path.replace('.js','.map');}, the change use staticDomain + '/javascript/lib/' + filenameWithoutExt + 'MIN.map'
+							 grunt.config.set('uglify.my_target' + i + '.options.sourceMap',function(path){return path.replace('.js','.map');});
+//							 grunt.config.set('uglify.my_target' + i + '.options.sourceMap',staticDomain + '/javascript/lib/' + filenameWithoutExt + 'MIN.map');
+							 grunt.config.set('uglify.my_target' + i + '.options.sourceMapRoot',staticDomain + '/javascript/concatJs/');
+//							 sourceMap set end
+							 
+							 grunt.config.set('uglify.my_target' + i + '.src',concatJsDest+ '/' + filenameWithoutExt + '.js');
+							 grunt.config.set('uglify.my_target' + i + '.dest',uglifiedJsDest + '/' + filenameWithoutExt + 'MIN.js');
+						 	 
 							 //in dev and prod environment,we can also store the uglified js files in javascript/lib
-							 destContents = destContents.replace(/\<duobei\:script.*?src="lib\/(.+).js.*?".*?\/\>/,'<duobei:script type="text/javascript" src="minlib/' + filenameWithoutExt + 'MIN.js" charset="utf-8"/>');
+							 destContents = destContents.replace(/lib\/(.+).js.*?"/,'libmin/' + filenameWithoutExt + 'MIN.js"');
 							 destContents = destContents.replace(/\<duobei:script.*?src="lib\/(.+).js.*?".*?\/\>/g,'');
 							 destContents = destContents.replace(/\<script.*?src=".*?lib\/(.+).js.*?".*?\>\<\/script\>/g,'');
-							 destContents = destContents.replace(/\/minlib\//,'/lib/');
+							 destContents = destContents.replace(/libmin\//g,'lib/'); 
 							 
 							 fs.writeFileSync(abspath, destContents, "UTF-8");
 							 grunt.log.ok("concat js files of " + abspath + " success"); 
 							 i = i+1;
-						}
-						 
+							 
+//							 if(i>200){
+//								 grunt.task.run('concat');
+//								 grunt.task.run('uglify');
+//								 i = 1;
+//							 }
+						 }
 					 }
 					 catch(e){
 						 grunt.log.error("compile js files of " + abspath + " fail");
@@ -65,6 +78,37 @@ module.exports = function(grunt){
 		
 		grunt.task.run('concat');
 		grunt.task.run('uglify');
+		
 		grunt.log.ok("concat and minify js finish");
 	});
+	
+	
+	grunt.task.registerMultiTask('changeSourceMapPath','change sourceMappingUrl in .js files and source in .map files', function(){
+		var staticSrc = grunt.config.get('changeSourceMapPath').targetSrc.staticSrc;
+		var staticDomain = grunt.config.get('changeSourceMapPath').targetSrc.staticDomain;
+		
+		grunt.file.recurse(staticSrc,function(abspath, rootdir, subdir, filename){
+			var extname = path.extname(filename);
+			
+			if(extname === '.map'){
+				var destContents = fs.readFileSync(abspath, "UTF-8");
+				var filenameWithoutExt = filename.replace(extname,'').replace('MIN','');
+				
+				destContents = destContents.replace(/\"sources\":\[\".*?\"\]/g,'"sources":["' + filenameWithoutExt + '.js"]');
+				fs.writeFileSync(abspath, destContents, "UTF-8");
+			}
+			
+			if(extname === '.js'){
+				var destContents = fs.readFileSync(abspath, "UTF-8");
+				var filenameWithoutExt = filename.replace(extname,'');
+				
+				destContents = destContents.replace(/sourceMappingURL=.*?\.map/g,'sourceMappingURL=' + staticDomain + '/javascript/lib/' + filenameWithoutExt + '.map');
+				fs.writeFileSync(abspath, destContents, "UTF-8");
+			}
+			
+		});
+		
+		grunt.log.ok("changeSourceMapPath finish");
+	});
+	
 };
